860cedb57e001a317b7406d7f7ecff5f7f13d732
SimPresko <preslibankov@gmail.com>
2024-08-15 17:04:16 +0300

    Lesson 3,4,5,6,7

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+//Modules
+const {john, peter} = require('./4-names.js');
+const sayHi = require('./5-utils.js');
+const data = require('./6-alternative-flavor.js');
+require('./7-mind-grenade.js');
+
+sayHi('susan');
+sayHi(john);
+sayHi(peter);
